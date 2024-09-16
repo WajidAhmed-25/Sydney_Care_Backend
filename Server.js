2439c6a7c92db675ca35798f23dc9c3ac773f9f4
WajidAhmed-25 <wajid.g20504@iqra.edu.pk>
2024-09-16 11:44:39 +0500

    serve

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -48,7 +48,8 @@ ${name} ${lastname} having contact number as: ${phone} has contacted you from th
 });
 
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
